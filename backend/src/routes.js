20577adc0ec6540830b35efb28760d15ed0d4249
thiago.co <thiago.co@empregos.com.br>
2019-08-06 11:48:06 -0300

    Like and Dislike Devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DisLikeController = require('./controllers/DisLikeController');
 
 const routes = express.Router();
 
@@ -7,6 +9,9 @@ routes.get('/', (req, res) => {
     return res.json({ message: `Hello Bacon ${req.query.name}`});
 });
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DisLikeController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-it4x2.mongodb.net/oministack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
-})
+});
 
+
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
