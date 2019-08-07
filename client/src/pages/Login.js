e1864c5cdf65a122a37e1baacd5d2f58a7c0b29c
thiago.co <thiago.co@empregos.com.br>
2019-08-07 09:42:34 -0300

    Start App React

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './../assets/logo.svg';
+
+
+export default function Login(){
+    return (        
+        <div>
+            <img src={logo} alt="Tindev" />
+        </div>
+    )
+}
+
